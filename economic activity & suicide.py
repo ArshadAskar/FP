75c32ee7090efb1b1b028f6014bb41e5a39825a1
ArshadAskar <ec22647@qmul.ac.uk>
2024-01-19 22:22:13 +0000

    removed comments and added new comments for plots

--- economic activity & suicide.py
+++ economic activity & suicide.py
@@ -17,37 +17,37 @@ emp = emp[(emp['Area name'] != 'Mean')]
 
 
 # Display the first few rows
-#print(emp)
+print(emp)
 
 #Plotting a line graph for suicide rates
-#plt.plot(smb['Area name'],smb['2005'],label=('2005'),marker='o')
-#plt.plot(smb['Area name'],smb['2009'],label=('2009'),marker='o')
-#plt.plot(smb['Area name'],smb['2014'],label=('2014'),marker='o')
-#plt.xticks(rotation=90)
+plt.plot(smb['Area name'],smb['2005'],label=('2005'),marker='o')
+plt.plot(smb['Area name'],smb['2009'],label=('2009'),marker='o')
+plt.plot(smb['Area name'],smb['2014'],label=('2014'),marker='o')
+plt.xticks(rotation=90)
 
 #labelling
-#plt.title("Graph showing suicide rates per 100000 people in all London boroughs")
-#plt.xlabel("Boroughs")
-#plt.ylabel("Suicide rate (per 100000)")
-#plt.subplots_adjust(bottom=0.3)  # The value is a fraction of the figure height.
-#plt.legend()
-#plt.show()
+plt.title("Graph showing suicide rates per 100000 people in all London boroughs")
+plt.xlabel("Boroughs")
+plt.ylabel("Suicide rate (per 100000)")
+plt.subplots_adjust(bottom=0.3)  # The value is a fraction of the figure height.
+plt.legend()
+plt.show()
 
 #Plotting a line graph for economically active people
-#plt.plot(emp['Area name'],emp['2005'],label=('2005'),marker='o')
-#plt.plot(emp['Area name'],emp['2009'],label=('2009'),marker='o')
-#plt.plot(emp['Area name'],emp['2014'],label=('2014'),marker='o')
-#plt.xticks(rotation=90)
+plt.plot(emp['Area name'],emp['2005'],label=('2005'),marker='o')
+plt.plot(emp['Area name'],emp['2009'],label=('2009'),marker='o')
+plt.plot(emp['Area name'],emp['2014'],label=('2014'),marker='o')
+plt.xticks(rotation=90)
 
 #labelling
-#plt.title("Graph showing economically active people in all London boroughs")
-#plt.xlabel("Boroughs")
-#plt.ylabel("Number of economically active people")
-#plt.subplots_adjust(bottom=0.3)  # The value is a fraction of the figure height.
-#plt.legend()
-#plt.show()
+plt.title("Graph showing economically active people in all London boroughs")
+plt.xlabel("Boroughs")
+plt.ylabel("Number of economically active people")
+plt.subplots_adjust(bottom=0.3)  # The value is a fraction of the figure height.
+plt.legend()
+plt.show()
 
-
+# Getting the data into a list
 camden_data = emp[emp['Area name'] == 'Camden']
 cam_index = camden_data.iloc[0,1:].tolist()
 camden_emp = cam_index
@@ -82,26 +82,19 @@ southwark_suicide_data = smb[smb['Area name'] == 'Southwark']
 southwa_index = southwark_suicide_data.iloc[0,1:].tolist()
 southwark_smb = southwa_index
 
-
 #Plotting a line graph for economically active people
-#plt.plot(years,camden_emp)
-#plt.xticks(rotation=90)
+plt.plot(years,camden_emp)
+plt.xticks(rotation=90)
 
 #labelling
-#plt.title("Graph showing number of economically active people in Camden")
-#plt.xlabel("Borough")
-#plt.ylabel("Number of economically active people")
-#plt.subplots_adjust(bottom=0.3)  # The value is a fraction of the figure height.
-#plt.legend()
-#plt.show()
+plt.title("Graph showing number of economically active people in Camden")
+plt.xlabel("Borough")
+plt.ylabel("Number of economically active people")
+plt.subplots_adjust(bottom=0.3)  # The value is a fraction of the figure height.
+plt.legend()
+plt.show()
 
-#labelling
-#plt.title("Graph showing economically active people in Camden")
-#plt.xlabel("Boroughs")
-#plt.ylabel("Employed people")
-#plt.legend()
-#plt.show()
-
+# Getting the data into a list
 mean_suicide_df = pd.read_csv(file_path1)
 mean_suicide = mean_suicide_df[mean_suicide_df['Area name'] == 'Mean']
 suic_index = mean_suicide.iloc[0,1:].tolist()
@@ -112,28 +105,29 @@ mean_employment = mean_emp_df[mean_emp_df['Area name'] == 'Mean']
 emp_index = mean_employment.iloc[0,1:].tolist()
 meanemp_emp = emp_index
 
-#plt.plot(years,camden_emp,label='Camden',marker='o')
-#plt.plot(years,meanemp_emp,label='London average',marker='o')
+#Plotting camden economically active and london average
+plt.plot(years,camden_emp,label='Camden',marker='o')
+plt.plot(years,meanemp_emp,label='London average',marker='o')
 
 #labelling
-#plt.title("Graph showing economically active people in Camden compared to the London average")
-#plt.xlabel("Time (Years)")
-#plt.ylabel("Number of economically active people")
-#plt.legend()
-#plt.show()
+plt.title("Graph showing economically active people in Camden compared to the London average")
+plt.xlabel("Time (Years)")
+plt.ylabel("Number of economically active people")
+plt.legend()
+plt.show()
 
-
-#plt.plot(years,islington_emp,label='Islington',marker='o')
-#plt.plot(years,westminster_emp,label='Westminster',marker='o')
-#plt.plot(years,Southwark_emp,label='Southwark',marker='o')
-#plt.plot(years,meanemp_emp,label='London average',marker='o')
+# Plotting HSB
+plt.plot(years,islington_emp,label='Islington',marker='o')
+plt.plot(years,westminster_emp,label='Westminster',marker='o')
+plt.plot(years,Southwark_emp,label='Southwark',marker='o')
+plt.plot(years,meanemp_emp,label='London average',marker='o')
 
 #labelling
-#plt.title("Graph showing economically active people in high suicide rate boroughs compared to the London average")
-#plt.xlabel("Time (Years)")
-#plt.ylabel("Number of economically active people")
-#plt.legend()
-#plt.show()
+plt.title("Graph showing economically active people in high suicide rate boroughs compared to the London average")
+plt.xlabel("Time (Years)")
+plt.ylabel("Number of economically active people")
+plt.legend()
+plt.show()
 
 
 # Mean for high suicide boroughs
@@ -153,20 +147,21 @@ for i in range(len(Mean_high_suicide3)):
 
 
 # Graph for HSB suicide rates and HSB economically active population
-#fig,ax1 = plt.subplots()
-#qual1,= ax1.plot(years,Mean_high_suicide4,marker='o',color = 'blue',label=("Suicide average"))
-#ax2 = ax1.twinx()
-#qual2, = ax2.plot(years,Mean_high_suicide2,marker='o',color ='red',label=("Economically active average"))
-#plt.xticks(years,rotation = "vertical")
+fig,ax1 = plt.subplots()
+qual1,= ax1.plot(years,Mean_high_suicide4,marker='o',color = 'blue',label=("Suicide average"))
+ax2 = ax1.twinx()
+qual2, = ax2.plot(years,Mean_high_suicide2,marker='o',color ='red',label=("Economically active average"))
+plt.xticks(years,rotation = "vertical")
 
 # Labelling
-#plt.title("Graph showing high suicide boroughs suicide rates (per 100,000) and their economically active population")
-#ax1.set_xlabel("Time (Years)")
-#ax1.set_ylabel('Suicide average (per 100,000)')
-#ax2.set_ylabel("Economically active average")
-#ax1.legend(handles=[qual1, qual2])
-#plt.show()
-    
+plt.title("Graph showing high suicide boroughs suicide rates (per 100,000) and their economically active population")
+ax1.set_xlabel("Time (Years)")
+ax1.set_ylabel('Suicide average (per 100,000)')
+ax2.set_ylabel("Economically active average")
+ax1.legend(handles=[qual1, qual2])
+plt.show()
+
+# Headline figure - comparing average london suicide and average economically active population    
 fig,ax1 = plt.subplots()
 qual3,= ax1.plot(years,meansuicide_smb,marker='o',color = 'black',label=("Suicide average"),linestyle='--')
 ax2 = ax1.twinx()
@@ -186,5 +181,3 @@ plt.show()
 corr = np.corrcoef(meanemp_emp,meansuicide_smb)[0,1]
 print(corr)
 
-### learn to put correlation coefficent inside the diagram -- headline figure
-## make it look nicer
