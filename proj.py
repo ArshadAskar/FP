af5daf0148a5dc18460e57de8c276d9b3c1ead4e
ArshadAskar <ec22647@qmul.ac.uk>
2024-01-19 10:06:51 +0000

    fixed filtering issue and un-commented the graph

--- proj.py
+++ proj.py
@@ -10,25 +10,27 @@ smb = pd.read_csv(file_path1)
 emp = pd.read_csv(file_path2)
 
 # Filter out rows for London
-smb =  smb[(smb['Area name'] != 'London') & smb['Area name'] != 'Mean']
-emp =  emp[(emp['Area name'] != 'London') & emp['Area name'] != 'Mean']
+smb = smb[(smb['Area name'] != 'London')]
+smb = smb[(smb['Area name'] != 'Mean')]
+emp = emp[(emp['Area name'] != 'London')]
+emp = emp[(emp['Area name'] != 'Mean')]
 
 
 # Display the first few rows
-print(smb)
+print(emp)
 
 #Plotting a line graph for suicide rates
-#plt.plot(smb['Area name'],smb['2009'],label=('2009'))
-#plt.plot(smb['Area name'],smb['2005'],label=('2005'))
-#plt.plot(smb['Area name'],smb['2014'],label=('2014'))
-#plt.xticks(rotation=90)
+plt.plot(smb['Area name'],smb['2009'],label=('2009'))
+plt.plot(smb['Area name'],smb['2005'],label=('2005'))
+plt.plot(smb['Area name'],smb['2014'],label=('2014'))
+plt.xticks(rotation=90)
 
 #labelling
-#plt.title("Graph showing suicide rates per 100000 people in all London boroughs")
-#plt.xlabel("Boroughs")
-#plt.ylabel("Suicide rate (per 100000)")
-#plt.legend()
-#plt.show()
+plt.title("Graph showing suicide rates per 100000 people in all London boroughs")
+plt.xlabel("Boroughs")
+plt.ylabel("Suicide rate (per 100000)")
+plt.legend()
+plt.show()
 
 #Plotting a line graph for economically active people
 #plt.plot(emp['Area name'],emp['2005'],label=('2005'))
